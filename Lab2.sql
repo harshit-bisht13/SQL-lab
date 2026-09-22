99c615d1e692198904ae491e1aca5c547a7b36fe
Hgafayagagag <harshitbisht1325@gmail.com>
2026-09-22 22:01:54 +0530

    Day 3 of Lab

--- Lab2.sql
+++ Lab2.sql
@@ -24,6 +24,17 @@ Drop table student;
 Insert into student(roll_no, name, course, contact, city) values
 	('S01','Harshit','B.tech',1234,'Bhimtal'),
     ('S02','Harsh','B.tech',4567,'Nainital'),
-    ('S03','Abhay','BCA',3456,'Bhimtal'),
-    ('S04','Aman','BBA',1334,'Bhimtal'),
-    ('S05','Arun','M.tech',1534,'Bhimtal');
+    ('S03','Abhay','BCA',3456,'Haldwani'),
+    ('S04','Aman','BBA',1334,'Dehradun'),
+    ('S05','Arun','M.tech',1534,'Haridwar');
+    select * from student;
+    Set sql_safe_updates=0;
+    update student set city='Nainital' where city='Bhimtal';
+    Delete from student where contact=1234;
+    Delete from student where city='Mumbai';
+    Delete from student where city='Delhi' or city = 'Dehradun';
+	Delete from student where city='Dehradun' and city = 'Nainital';
+	truncate student; 
+    select* from student;
+    Describe student;
+    Drop table student;
