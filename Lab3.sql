e58c9e3d85c317b3f23460f18a51455a71bf247b
Hgafayagagag <harshitbisht1325@gmail.com>
2026-09-25 20:32:40 +0530

    Fifth day of Lab

--- Lab3.sql
+++ Lab3.sql
@@ -11,3 +11,16 @@ use college;
         );
         Describe student;
         Drop table student;
+        Insert into student values
+			('S01','Amit','1234','abc@gmail.com','Delhi',66,'BCA'),
+            ('S02','Binay','2345','Bcd@gmail.com','Delhi',78,'Btech'),
+            ('S03','Chitra','3456','cde@gmail.com','Bhimtal',85,'Btech'),
+            ('S08','Deepak','4567','def@gmail.com','Bhimtal',73,'MCA'),
+            ('S04','Garima','5678','efg@gmail.com','Nainital',75,'BCA'),
+            ('S05','Lolit','6789','ghi@gmail.com','Dehradun',91,'Btech'),
+            ('S06','Yash','7890','hij@gmail.com','Delhi',86,'Btech');
+Insert into student values
+('','Shubham','8901','klm@gmail.com','Haridwar','78','Mtech'),
+('S09','Shubham Rawat','','ijk@gmail.com','Haridwar','78','Mtech');
+Insert into student values
+('S10','Navv','','jkl@gmail.com','Haridwar','78','Mtech');
